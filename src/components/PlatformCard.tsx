37e17150a991179de3d7047bef24c01ad2f80ea3
Linus-Daniel <vandulinus@gmail.com>
2025-04-13 10:26:28 +0100

    First commit

--- src/components/PlatformCard.tsx
+++ src/components/PlatformCard.tsx
@@ -11,7 +11,7 @@ interface PlatformCardProps {
 const PlatformCard: React.FC<PlatformCardProps> = ({ icon, title, description, downloadLink }) => {
   return (
     <div className="flex flex-col gap-4 p-6 bg-white rounded-xl shadow hover:shadow-lg transition-shadow w-full sm:w-[300px]">
-      <Image alt='icon' src={icon}/>
+      <Image alt='icon' src={icon as string}/>
       <h3 className="text-lg text-gray font-semibold">{title}</h3>
       <p className="text-sm text-gray-600">{description}</p>
       <a href={downloadLink} className="text-blue-600 underline mt-auto w-fit">
